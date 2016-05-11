4b8e960d3eecb56794a1d5ff253fb4f809f6e16e
piji <piji@piji>
2016-05-10 17:35:57 -0700

    second commit

--- studyEclipse/src/studyEclipsepackage/CheckPrime.java
+++ studyEclipse/src/studyEclipsepackage/CheckPrime.java
@@ -3,25 +3,17 @@ package studyEclipsepackage;
 public class CheckPrime {
 	int num = 13;
 	
-	public int isPrime(int num)
-	{
+	public int isPrime(int num) {
 		int j = 0;
-		for (int i =1; i<=num/2;i++)
-		{
-		j = num%i;
-		
+		for (int i =1; i<=num/2;i++) {
+			j = num%i;
 		}
-		if (j!=0)
-		{	
-				
-		return 1;
-		}
-		else
-		{
+		if (j!=0) {			
+			return 1;
+		} else {
 			return 0;
 		}
 	}
-	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
